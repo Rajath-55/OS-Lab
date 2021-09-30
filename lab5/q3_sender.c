01f791714e3ba94c4cd540d9d4797adffd3c0fa5
Rajath-55 <f20190122@goa.bits-pilani.ac.in>
2021-09-30 16:12:42 +0530

    Updated lab 5

---
+++ lab5/q3_sender.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#define MAX 100
+  
+// structure for message queue
+struct mesg_buffer {
+    long mesg_type;
+    char mesg_text[100];
+} message;
+  
+int main()
+{
+    key_t key;
+    int msgid;
+  
+    // ftok to generate unique key
+    key = ftok("progfile", 65);
+  
+    // msgget creates a message queue
+    // and returns identifier
+    msgid = msgget(key, 0666 | IPC_CREAT);
+    message.mesg_type = 1;
+  
+    printf("Write Data : ");
+    fgets(message.mesg_text,MAX,stdin);
+  
+    // msgsnd to send message
+    msgsnd(msgid, &message, sizeof(message) + 1, 0);
+  
+  
+    return 0;
+}
